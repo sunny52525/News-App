8d198e265c48150a1ed1b5ab8352b4f871ec4cbe
Sunny <30768018+sunny52525@users.noreply.github.com>
2020-06-26 11:12:15 +0530

    Added Features and Fixed Bugs

---
+++ app/src/main/java/com/shaun/news/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.shaun.news
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+ class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT = 1000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(android.R.style.Theme_DeviceDefault_NoActionBar)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed(Runnable { // This method will be executed once the timer is over
+            // Start your app main activity
+            val i = Intent(
+                this,
+                MainActivity::class.java
+            )
+            startActivity(i)
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
